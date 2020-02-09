6ab853bd78dcb76c5ea89b72d14360da3d28225a
Kamocuvao <kamocuvao@gmail.com>
2020-02-09 11:04:47 +0100

    feat: add task active flag and style

--- src/components/Task.svelte
+++ src/components/Task.svelte
@@ -12,14 +12,33 @@
   function handleClick() {
     console.log("clicked");
   }
+
+  export let title = "Title";
+  export let subtitle = "This is a subtitle.";
+  export let image = "https://via.placeholder.com/280x380.png?text=280x380";
+  export let active = false;
 </script>
 
 <style>
+  .mediacontent {
+    display: grid;
+    justify-items: stretch;
+    height: 380px;
+    grid-template-columns: 1fr;
+    grid-template-rows: min-content min-content auto min-content;
+  }
+
   .text {
     color: #fff;
-    position: absolute;
-    bottom: 16px;
-    left: 16px;
+    text-shadow: 0 0 5px black;
+    text-align: center;
+  }
+
+  .active {
+    background-color: darkslateblue;
+    color: #fff;
+    text-align: center;
+    grid-row: 4;
   }
 
   h2,
@@ -28,16 +47,17 @@
   }
 </style>
 
-<Card style="width: 320px;">
+<Card style="width:280px;">
   <PrimaryAction on:click={handleClick()}>
-    <Media
-      style="background-image:
-      url(https://via.placeholder.com/320x320.png?text=square);"
-      aspectRatio="square">
-      <div class="text">
-        <h2 class="mdc-typography--headline6">A card with square media.</h2>
-        <h3 class="mdc-typography--subtitle2">And a subtitle.</h3>
+    <Media style="background-image: url({image}); height:380px;">
+      <div class="mediacontent">
+        <h2 class="text mdc-typography--headline6">{title}</h2>
+        <h3 class="text mdc-typography--subtitle2">{subtitle}</h3>
+        {#if active}
+          <h2 class="active mdc-typography--headline6">Active</h2>
+        {/if}
       </div>
     </Media>
+
   </PrimaryAction>
 </Card>
--- src/routes/classes.svelte
+++ src/routes/classes.svelte
@@ -2,8 +2,16 @@
   import Task from "../components/Task.svelte";
 </script>
 
+<style>
+  .tasks {
+    display: flex;
+  }
+</style>
+
 <svelte:head>
   <title>Classes</title>
 </svelte:head>
-
-<Task />
+<div class="tasks">
+  <Task active="true" />
+  <Task />
+</div>
