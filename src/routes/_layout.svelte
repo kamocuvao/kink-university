0f89083f86394d94befbd2c0de34d5159a7b886f
Kamocuvao <kamocuvao@gmail.com>
2020-03-29 15:41:02 +0200

    refactor: improve page structure

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,9 +1,201 @@
 <script>
-  import Nav from "../components/Nav.svelte";
+  import { onMount } from "svelte";
+  import { stores } from "@sapper/app";
+  import TopAppBar, { Row, Section, Title } from "@smui/top-app-bar";
+  import IconButton from "@smui/icon-button";
+  import List, { Item, Text, Graphic, Separator, Subheader } from "@smui/list";
+  import Drawer, {
+    AppContent,
+    Content,
+    Header,
+    Title as DrawerTitle,
+    Subtitle,
+    Scrim
+  } from "@smui/drawer";
+  import H6 from "@smui/common/H6.svelte";
+
+  let mainContent;
+  let drawer;
+  let drawerOpen = false;
+  let active = "Home";
+  let miniWindow = false;
+
+  const { page } = stores();
+
+  function pickSection(section) {
+    mainContent.scrollTop = 0;
+    drawerOpen = false;
+
+    // Svelte/Sapper is not updated the components correctly, so I need this.
+    sections
+      .concat(helpSections)
+      .forEach(section => section.component.$set({ activated: false }));
+    section.component.$set({ activated: true });
+  }
+
+  const sections = [
+    {
+      name: "Home",
+      route: "/",
+      icon: "home",
+      disabled: false
+    },
+    {
+      name: "Majors",
+      route: "/majors",
+      icon: "star",
+      disabled: true
+    },
+    {
+      name: "Classes",
+      route: "/classes",
+      icon: "school",
+      disabled: false
+    },
+    {
+      name: "Clubs/Partners",
+      route: "/clubs-partners",
+      icon: "people",
+      disabled: true
+    },
+    {
+      name: "Punishments",
+      route: "/punishments",
+      icon: "sports",
+      disabled: true
+    },
+    {
+      name: "Schedule",
+      route: "/schedule",
+      icon: "schedule",
+      disabled: true
+    }
+  ];
+
+  const helpSections = [
+    {
+      name: "Help/Rules",
+      route: "/help",
+      icon: "help",
+      disabled: false
+    },
+    {
+      name: "Settings",
+      route: "/settings",
+      icon: "settings",
+      disabled: false
+    }
+  ];
+
+  onMount(setMiniWindow);
+
+  function setMiniWindow() {
+    miniWindow = window.innerWidth < 720;
+  }
 </script>
 
-<Nav />
+<style>
+  .drawer-container {
+    display: grid;
+    grid-template-columns: min-content auto;
+    height: 100%;
+  }
 
-<main>
-  <slot />
-</main>
+  .demo-main-content {
+    height: 100%;
+  }
+</style>
+
+<svelte:window on:resize={setMiniWindow} />
+
+<TopAppBar variant="static" color="primary">
+  <Row>
+    <Section>
+      {#if miniWindow}
+        <IconButton
+          class="material-icons"
+          on:click={() => (drawerOpen = !drawerOpen)}>
+          menu
+        </IconButton>
+      {/if}
+      <Title>Kink University</Title>
+    </Section>
+    <Section align="end" toolbar>
+      <IconButton
+        class="material-icons"
+        aria-label="Download"
+        href="settings"
+        on:click={() => pickSection(helpSections[1])}>
+        file_download
+      </IconButton>
+      <IconButton class="material-icons" aria-label="Bookmark this page">
+        bookmark
+      </IconButton>
+    </Section>
+  </Row>
+</TopAppBar>
+
+<div class="drawer-container">
+  <Drawer
+    class="demo-drawer"
+    variant={miniWindow ? 'modal' : null}
+    bind:this={drawer}
+    bind:open={drawerOpen}>
+    <Content>
+      <List>
+        {#each sections as section (section.name)}
+          <Item
+            bind:this={section.component}
+            href={'route' in section ? section.route : section.shortcut}
+            on:click={() => pickSection(section)}
+            activated={'route' in section && section.route === $page.path}
+            disabled={section.disabled}>
+            <Graphic class="material-icons" aria-hidden="true">
+              {section.icon}
+            </Graphic>
+            <Text>
+              {#if section.disabled}
+                <span style="color:grey">{section.name}</span>
+              {:else}{section.name}{/if}
+            </Text>
+          </Item>
+        {/each}
+
+        <Separator nav />
+        <Subheader component={H6}>Help</Subheader>
+        {#each helpSections as section (section.name)}
+          <Item
+            bind:this={section.component}
+            href={'route' in section ? section.route : section.shortcut}
+            on:click={() => pickSection(section)}
+            activated={'route' in section && section.route === $page.path}
+            disabled={section.disabled}>
+            <Graphic class="material-icons" aria-hidden="true">
+              {section.icon}
+            </Graphic>
+            <Text>
+              {#if section.disabled}
+                <span style="color:grey">{section.name}</span>
+              {:else}{section.name}{/if}
+            </Text>
+          </Item>
+        {/each}
+
+      </List>
+    </Content>
+  </Drawer>
+
+  {#if miniWindow}
+    <!--This is so that the css grid min.content on a small window works-->
+    <div />
+
+    <Scrim />
+  {/if}
+
+  <AppContent class="demo-app-content">
+    <main class="demo-main-content" bind:this={mainContent}>
+      <slot />
+    </main>
+  </AppContent>
+
+</div>
--- src/stores.js
+++ src/stores.js
@@ -1,57 +1,54 @@
 import { writable, readable } from "svelte/store";
+import classesData from "./data/classes";
 
 export const TaskState = Object.freeze({
   inactive: 1,
   active: 2,
-  completed: 3
+  completed: 3,
+  toString(state) {
+    switch (state) {
+      case 1:
+        return "inactive";
+      case 2:
+        return "active";
+      case 3:
+        return "completed";
+      default:
+        return "unknown";
+    }
+  }
 });
 
-const classesData = [
-  {
-    id: 1,
-    title: "Fluids 101",
-    subtitle: "enema watersports",
-    description: "You will learn to play with different liquids."
-  },
-  {
-    id: 2,
-    title: "Fluids 101",
-    subtitle: "enema watersports",
-    description: "You will learn to play with different liquids."
-  },
-  {
-    id: 3,
-    title: "Fluids 101",
-    subtitle: "enema watersports",
-    description: "You will learn to play with different liquids."
-  },
-  {
-    id: 4,
-    title: "Fluids 101",
-    subtitle: "enema watersports",
-    description: "You will learn to play with different liquids."
-  },
-  {
-    id: 5,
-    title: "Fluids 101",
-    subtitle: "enema watersports",
-    description: "You will learn to play with different liquids."
-  },
-  {
-    id: 6,
-    title: "Fluids 101",
-    subtitle: "enema watersports",
-    description: "You will learn to play with different liquids."
-  }
-];
-
 const classStateData = {
-  1: TaskState.inactive,
-  2: TaskState.inactive,
-  3: TaskState.inactive,
-  4: TaskState.inactive,
-  5: TaskState.inactive,
-  6: TaskState.inactive
+  101: TaskState.inactive,
+  102: TaskState.inactive,
+  103: TaskState.inactive,
+  104: TaskState.inactive,
+  105: TaskState.inactive,
+  106: TaskState.inactive,
+  107: TaskState.inactive,
+  108: TaskState.inactive,
+  109: TaskState.inactive,
+  110: TaskState.inactive,
+  201: TaskState.inactive,
+  202: TaskState.inactive,
+  203: TaskState.inactive,
+  204: TaskState.inactive,
+  205: TaskState.inactive,
+  206: TaskState.inactive,
+  207: TaskState.inactive,
+  208: TaskState.inactive,
+  209: TaskState.inactive,
+  210: TaskState.inactive,
+  301: TaskState.inactive,
+  302: TaskState.inactive,
+  303: TaskState.inactive,
+  304: TaskState.inactive,
+  305: TaskState.inactive,
+  306: TaskState.inactive,
+  307: TaskState.inactive,
+  308: TaskState.inactive,
+  309: TaskState.inactive
 };
 
 export const classes = readable(classesData);
